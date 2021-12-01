3c5c3e8d3b9078d60a71b15b7efb6d7e9d8e7698
Maxime VINCENT <maxime2.vincent@epitech.eu>
2021-12-01 15:44:17 +0100

    Added D01

---
+++ d01/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+const countHigher = (highs) => highs.reduce((acc, curr, i, all) => i > 0 && curr > all [i - 1] ? acc + 1 : acc, 0);
+const getNums = (data) => data.split('\n').filter((l) => l.length > 0).map((l) => parseInt(l, 10))
+const concatHighs = (highs) => highs.reduce((acc, c, i, arr) => (i > arr.length - 3) ? acc :[...acc, [arr[i], arr[i + 1], arr[i + 2]].reduce((sum, n) => sum + n, 0)], [])
+
+fs.readFile('./input', 'utf8', (err, data) => {
+  console.log(countHigher(getNums(data)))
+  console.log(countHigher(concatHighs(getNums(data))))
+})
